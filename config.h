be4e75e62e2c48538fa7a107292f1028ca609e51
jumps <jumpsareop@gmail.com>
2023-08-30 13:39:17 +0300

    Fix CPU calculation errors on some systems

--- config.h
+++ config.h
@@ -62,7 +62,7 @@ DEFFUN(getcpu){
 		return;
 
 	swprintf(b, BLKBUFSIZ, L"%llu", (100*((x[0]+x[1]+x[2]+x[5]+x[6])-
-	                                (y[0]+y[1]+y[2]+y[6])))/sum);
+	                                (y[0]+y[1]+y[2]+y[5]+y[6])))/sum);
 }
 
 DEFFUN(getmem){
